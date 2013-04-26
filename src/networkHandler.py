60a16869b5c507af894aa2bdeb8f921f23afb186
Gregory COlella <gregory.colella@gmail.com>
2013-04-25 20:58:41 -0400

    Began commenting networkHandler

--- src/networkHandler.py
+++ src/networkHandler.py
@@ -6,46 +6,77 @@ import Commands_gen
 from ctypes import cdll
 from Commands_gen import *
 
+# This is the library that wraps Kovan API functions into a shared object library,
+# ctypes allows us to make calls to robot functions through python.
 lib = cdll.LoadLibrary('./libControl.so')
 
+
+# Tells the server to return a line after each command is processed, allowing the
+# client to know not to send more lines than can be processed at once
+# (Not yet implemented in client-side code.)
 SERVERANSWER = False
 
+# Port number, if you edit this make sure you edit it in client-side code as well.
 PORT_NUM = 7777
-writebuffer = ""
 
+
+
+# Outfile opens if a command-line argument was passed to it 
 outfile = ''
 if(len(sys.argv) > 1):
 	outfile = sys.argv[1]
 	print 'Network handler writing to: ', outfile
 
 
+# Handles the parsing  of a command line and returns the Command object representing
+# the command sent to it.
 def handleLine(thisLine):
 	thisLine = thisLine.split()
         thisPrefix = thisLine[0].lower()
+
+	# Remove the string prefix from the command line
         thisLine.pop(0)
+
+	# Parse integers out of the rest of the command line
         theseArgs = [int(i) for i in thisLine]
 
+	# Just some echo output
 	print thisPrefix + " " + str(theseArgs)
 
+	return getCommand(thisPrefix, theseArgs)
+
+# Gets the command represented by a given prefix with the arguments from an int array
+def getCommand(prefix, args):
+	# Uses the dictionary in the generated Commands_gen file to look up the
+	# command class, then instantiates and populates a new instance
 	if thisPrefix in Commands_gen.commandSet:
 		thisCommand = Commands_gen.commandSet[thisPrefix]()
-		thisCommand.create_from(theseArgs)
+		thisCommand.create_from(args)
 		return thisCommand
 
+	# If the prefix isn't found, simply return a command that does nothing and 
+	# represents no text
+	return Command()	
 
-	return Command()
 
+# Start of execution code
 print 'Accepting commands..'
 
+# Open a standard server socket
 thisSocket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+
+# This just lets us use the same port even if the last server didn't cleanly exit without a timeout
 thisSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR,1)
+
+# Bind the server to the port
 thisSocket.bind( ('',PORT_NUM) )
+
+# Allow incoming connections
 thisSocket.listen(1)
 
 if(outfile):
 	outFile = open('/kovan/binaries/' + outfile.strip()+'.c','w')
-	outFile.write('int main() {\n wait_for_light(0); \n shut_down_in(110); \n')
-#	writebuffer += ('int main() {\n')
+	outFile.write('int main() {\n wait_for_light(0); \n shut_down_in(110); \
 
 
 
@@ -53,11 +84,7 @@ def writeCommand(command, ds):
 	if(outfile):
 		outFile.write("msleep("+str(ds)+");\n")
 		outFile.write(command.toCommands())
-		#outFile.flush()
-	#	global writebuffer
-	#	writebuffer.append("msleep("+str(ds)+");\n")
-	#	writebuffer+=command.toCommands()
-
+		
 
 channel, details = thisSocket.accept()
 channelFile = channel.makefile()
@@ -79,7 +106,6 @@ while True:
 	command.execute()
 
 if( outfile ):
-#	outFile.write(writebuffer)
 	outFile.write('\nreturn 0;\n}')
 	outFile.flush()
 	outFile.close()
